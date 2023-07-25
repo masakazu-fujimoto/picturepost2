0a1ac1f5dec9c10dcac66d3fc151b38638d9bb88
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-07-25 17:32:59 +0900

    createアクション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.all
   end
@@ -6,4 +7,14 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
 end
