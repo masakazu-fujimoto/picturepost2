39389118915429e84b7ece51278bf32e8badd9a9
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-08-04 22:30:43 +0900

    テストコード編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.nickname = ''  # nicknameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.email = ''  # emailの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Email can't be blank"
+    end
+  end
 end
